5e2a84fdc4808b0bb3b623df4385f64f2bb5c0be
Lucas Feliciano <lucas1feliciano0@gmail.com>
2022-04-17 17:09:20 -0300

    feat: http-client

---
+++ src/infra/http-client/axios-http-client.ts
@@ -0,0 +1,28 @@
+import axios, {AxiosInstance, AxiosResponse} from 'axios';
+import {IHttpClient, HttpRequest, HttpResponse} from '../../domain/gateways';
+
+export class AxiosHttpClient implements IHttpClient {
+  private api: AxiosInstance;
+
+  constructor(baseURL: string) {
+    this.api = axios.create({baseURL});
+  }
+
+  async request(data: HttpRequest): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse;
+    try {
+      axiosResponse = await this.api.request({
+        url: data.url,
+        method: data.method,
+        data: data.body,
+        params: data.params,
+      });
+    } catch (error: any) {
+      axiosResponse = error.response;
+    }
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    };
+  }
+}
