de66a38f6cabe604d1cc6af560b69268600cd766
Lucas Feliciano <lucas1feliciano0@gmail.com>
2022-04-19 16:55:28 -0300

    env: implemented .env file

--- src/main/factories/usecases/remote-get-weather.ts
+++ src/main/factories/usecases/remote-get-weather.ts
@@ -1,4 +1,6 @@
 import {AxiosHttpClient} from '../../../infra/http-client/axios-http-client';
+import Config from 'react-native-config';
+
 import {OWMWeatherRepository} from '../../../infra/repositories/open-weather-map/remote-weather-repository';
 import {SearchWeather} from '../../../domain/usecases/get-weather';
 
@@ -6,10 +8,7 @@ const makeRemoteSearchWeather = (): SearchWeather => {
   const httpClient = new AxiosHttpClient(
     'https://api.openweathermap.org/data/2.5',
   );
-  const weatherRepo = new OWMWeatherRepository(
-    httpClient,
-    'bb59b86b3c3e8d60cd1c91e639520982',
-  );
+  const weatherRepo = new OWMWeatherRepository(httpClient, Config.OWM_APP_ID);
   const searchWeather = new SearchWeather(weatherRepo);
 
   return searchWeather;
--- src/main/factories/usecases/remote-search-city.ts
+++ src/main/factories/usecases/remote-search-city.ts
@@ -1,10 +1,13 @@
+import Config from 'react-native-config';
+
 import {AxiosHttpClient} from './../../../infra/http-client/axios-http-client';
 import {GPACityRepository} from './../../../infra/repositories/google-places-api/remote-city-repository';
 import {SearchCity} from '../../../domain/usecases/search-city';
 
 const makeRemoteSearchCity = (): SearchCity => {
   const baseUrl = 'https://maps.googleapis.com/maps/api';
-  const key = 'AIzaSyAwP_dC9dkXRRO0-bJM4id7vQfK-k2TeEo';
+  const key = Config.GPA_API_KEY;
+
   const httpClient = new AxiosHttpClient(baseUrl);
   const cityRepo = new GPACityRepository(httpClient, key);
   const searchCity = new SearchCity(cityRepo);
